0a4de804462403959da675d6cc43fa496c8e79d8
Juan F. Pérez <j.guitar@gmail.com>
2014-12-15 09:26:33 +0100

    with fasd integration and z alias, it is useless

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -1,6 +1,2 @@
 alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber -p travis"
 alias ngrok="~/Downloads/ngrok -subdomain jguitar 3000"
-
-alias dfr="cd ~/Documents/source/dotfiles"
-alias bkb="cd ~/Documents/source/barkibu-platform"
-alias adp="cd ~/Documents/source/adopt-a-pet"
