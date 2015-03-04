0f4bcbb2bdf10ceecfb1f717cd7184fd2bd5c9f8
Juan F. Pérez <j.guitar@gmail.com>
2015-03-04 09:15:59 +0100

    ngk is a better alias than rename it

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -1,2 +1,2 @@
 alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber -p travis"
-alias ngrok="~/Downloads/ngrok -subdomain jguitar 3000"
+alias ngk="~/Downloads/ngrok -subdomain jguitar 3000"
