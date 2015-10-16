6914ddda58af7a91d3e13107d5d1a0e4d5869e32
Juan F. Pérez <j.guitar@gmail.com>
2015-10-16 10:38:16 +0200

    added gcf

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -2,3 +2,4 @@ alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber 
 alias ngk="~/Downloads/ngrok -subdomain jguitar 3000"
 eval "$(hub alias -s)"
 export ANDROID_HOME=/usr/local/opt/android-sdk
+alias gcf="git commit --fixup"
