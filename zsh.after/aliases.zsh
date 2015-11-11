5d098a19b0ee51bb775a3a9dfc5ce5409f0bb756
Juan F. Pérez <j.guitar@gmail.com>
2015-11-11 16:21:11 +0100

    playing again with Android

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -1,5 +1,6 @@
 alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber -p travis"
 alias ngk="~/Downloads/ngrok -subdomain jguitar 3000"
 eval "$(hub alias -s)"
-export ANDROID_HOME=/usr/local/opt/android-sdk
+export ANDROID_HOME=~/Library/Android/sdk
+export PATH="$ANDROID_HOME/tools:$PATH"
 alias gcf="git commit --fixup"
