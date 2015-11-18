5192a4f1f134e3b188ce91eea333c6ed918ffd4c
Juan F. Pérez <j.guitar@gmail.com>
2015-11-18 13:22:05 +0100

    update ngrok to 2.0

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -1,5 +1,5 @@
 alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber -p travis"
-alias ngk="~/Downloads/ngrok -subdomain jguitar 3000"
+alias ngk="~/Downloads/ngrok http -subdomain=jguitar 3000"
 eval "$(hub alias -s)"
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH="$ANDROID_HOME/tools:$PATH"
