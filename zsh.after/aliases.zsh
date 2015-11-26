60ba0eca7439d067b76eb6e2498c12f5e2d00d0b
Juan F. Pérez <j.guitar@gmail.com>
2015-11-26 11:12:38 +0100

    add android platform-tools to PATH

--- zsh.after/aliases.zsh
+++ zsh.after/aliases.zsh
@@ -2,5 +2,5 @@ alias phantom="phantomjs --webdriver=8910 & PHANTOMJS=true bundle exec cucumber 
 alias ngk="~/Downloads/ngrok http -subdomain=jguitar 3000"
 eval "$(hub alias -s)"
 export ANDROID_HOME=~/Library/Android/sdk
-export PATH="$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 alias gcf="git commit --fixup"
