2b4716ee391cef24e1c3bf0dae027bb37621aec0
Juan F. Pérez <j.guitar@gmail.com>
2017-04-20 14:37:51 +0200

    add sopra tools

---
+++ zsh.after/dev_tools.zsh
@@ -0,0 +1,11 @@
+# nvm setup
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# pyenv setup
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# virtualenv setup
+export WORKON_HOME="$HOME/.virtualenvs"
