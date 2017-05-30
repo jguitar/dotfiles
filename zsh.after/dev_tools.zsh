64177e1d386364b172d14a4082f4b5f5da5be7b5
Juan F. Pérez <j.guitar@gmail.com>
2017-05-30 16:59:32 +0200

    save android setup

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -10,3 +10,8 @@ source /usr/local/bin/virtualenvwrapper.sh
 export PATH=~/.gem/ruby/2.3.0/bin:$PATH
 
 source ~/.bin/tmuxinator.zsh
+
+# android sdk and some tools
+export ANDROID_HOME=~/Android/Sdk
+export PATH=ANDROID_HOME:$PATH
+export PATH=$PATH:/usr/local/jdk1.8.0_131/bin:/usr/local/android-studio/gradle/gradle-3.2/bin
