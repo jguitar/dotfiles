6f913507de35ea4916c3de6fe3d175256eb7919f
Juan F. Pérez <j.guitar@gmail.com>
2017-05-30 16:59:15 +0200

    remove pyenv (not used)

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -2,11 +2,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# pyenv setup
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
 # virtualenv setup
 export WORKON_HOME="$HOME/.virtualenvs"
 source /usr/local/bin/virtualenvwrapper.sh
