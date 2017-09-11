f44acf4f290a3f0d999d0ccc880dbb861452cb2b
Juan F. Pérez <j.guitar@gmail.com>
2017-09-11 15:24:34 +0200

    wip

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -3,9 +3,17 @@ export NVM_DIR="$HOME/.nvm"
 . "/usr/local/opt/nvm/nvm.sh"
 
 # ruby gems as executable
-export PATH=~/.gem/ruby/2.3.0/bin:$PATH
+# export PATH=~/.gem/ruby/2.3.0/bin:$PATH
 
-# android sdk and some tools
-export ANDROID_HOME=~/Android/Sdk
-export PATH=ANDROID_HOME:$PATH
-export PATH=$PATH:/usr/local/jdk1.8.0_131/bin:/usr/local/android-studio/gradle/gradle-3.2/bin
+# Create a JAVA_HOME variable, determined dynamically
+export JAVA_HOME=$(/usr/libexec/java_home)
+# Add that to the global PATH variable
+export PATH=${JAVA_HOME}/bin:$PATH
+# Set Android_HOME
+export ANDROID_HOME=~/Library/Android/sdk/
+# Add the Android SDK to the ANDROID_HOME variable
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$PATH
+#Set GRADLE_HOME
+# export GRADLE_HOME=/Library/gradle/gradle-3.2
+# export PATH=$PATH:$GRADLE_HOME/bin
