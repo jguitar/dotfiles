6cd252650da141301ff28312a5dcfb9e2a855ccb
Juan F. Pérez <j.guitar@gmail.com>
2018-02-06 10:32:46 +0100

    fix android setup

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -26,8 +26,9 @@ load-nvmrc
 # Java setup with Android Studio
 export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home
 export PATH=${JAVA_HOME}/bin:$PATH
-export ANDROID_HOME=~/Library/Android/sdk/
+export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/tools/bin:$PATH
 export GRADLE_HOME=/Applications/Android\ Studio.app/Contents/gradle/gradle-3.2
 export PATH=$PATH:$GRADLE_HOME/bin
