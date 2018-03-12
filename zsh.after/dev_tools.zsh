4974c7666b372838a7c22caaee7f2d30ca4aee24
Juan F. Pérez <j.guitar@gmail.com>
2018-03-12 09:16:38 +0100

    add build-tools

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -29,6 +29,7 @@ export PATH=${JAVA_HOME}/bin:$PATH
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$ANDROID_HOME/platform-tools:$PATH
 export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/build-tools/27.0.3:$PATH
 export PATH=$ANDROID_HOME/tools/bin:$PATH
 export GRADLE_HOME=/Applications/Android\ Studio.app/Contents/gradle/gradle-3.2
 export PATH=$PATH:$GRADLE_HOME/bin
