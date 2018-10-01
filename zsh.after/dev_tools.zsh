4b7753adf758278a118c51d11f3589cbb5c3e0b0
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2018-10-01 16:40:56 +0200

    add Qt

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -8,3 +8,6 @@ export PATH=$ANDROID_HOME/build-tools/27.0.3:$PATH
 export PATH=$ANDROID_HOME/tools/bin:$PATH
 export GRADLE_HOME=/Applications/Android\ Studio.app/Contents/gradle/gradle-3.2
 export PATH=$PATH:$GRADLE_HOME/bin
+
+# Qt
+export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
