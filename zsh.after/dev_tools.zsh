125c9c3704c9ccc14dafcab339b3d6631d032fa3
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2020-01-09 12:39:31 +0100

    fix some problem with Ruby and Mac

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -11,3 +11,6 @@ export PATH=$PATH:$GRADLE_HOME/bin
 
 # Qt
 export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
+
+# Some fix: https://blog.phusion.nl/2017/10/13/why-ruby-app-servers-break-on-macos-high-sierra-and-what-can-be-done-about-it/
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
