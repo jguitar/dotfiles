bad676a6dc29973e896c0a2a951a52efef748fa5
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2020-09-08 09:06:50 +0200

    export Flutter

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -14,3 +14,5 @@ export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
 
 # Some fix: https://blog.phusion.nl/2017/10/13/why-ruby-app-servers-break-on-macos-high-sierra-and-what-can-be-done-about-it/
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+
+export PATH=$PATH:~/Documents/source/flutter/bin
