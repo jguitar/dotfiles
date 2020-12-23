fd9d7533eb10cb91f52c35cc1fdeddc86109ad8c
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2020-12-23 11:33:09 +0100

    add Flutter

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -15,4 +15,7 @@ export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
 # Some fix: https://blog.phusion.nl/2017/10/13/why-ruby-app-servers-break-on-macos-high-sierra-and-what-can-be-done-about-it/
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
+# Flutter and tools (e.g. Webdev)
 export PATH=$PATH:~/Documents/source/flutter/bin
+export PATH=$PATH:~/Documents/source/flutter/bin/cache/dart-sdk/bin
+export PATH=$PATH:$HOME/.pub-cache/bin
