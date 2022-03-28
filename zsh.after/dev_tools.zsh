b839decb48771b4f9a383bca27f4fcfc30f86d10
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2022-03-28 11:58:43 +0200

    remove Java

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -1,14 +1,3 @@
-# Java setup with Android Studio
-export JAVA_HOME=/Applications/Android\ Studio.app/Contents/jre/jdk/Contents/Home
-export PATH=${JAVA_HOME}/bin:$PATH
-export ANDROID_HOME=~/Library/Android/sdk
-export PATH=$ANDROID_HOME/platform-tools:$PATH
-export PATH=$ANDROID_HOME/tools:$PATH
-export PATH=$ANDROID_HOME/build-tools/27.0.3:$PATH
-export PATH=$ANDROID_HOME/tools/bin:$PATH
-export GRADLE_HOME=/Applications/Android\ Studio.app/Contents/gradle/gradle-3.2
-export PATH=$PATH:$GRADLE_HOME/bin
-
 # Qt
 export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
 
@@ -19,9 +8,3 @@ export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 export PATH=$PATH:~/Documents/source/flutter/bin
 export PATH=$PATH:~/Documents/source/flutter/bin/cache/dart-sdk/bin
 export PATH=$PATH:$HOME/.pub-cache/bin
-
-# Set openjdk first in the PATH
-export PATH=/usr/local/opt/openjdk/bin:$PATH
-
-# Let compilers to find openjdk
-export CPPFLAGS="-I/usr/local/opt/openjdk/include"
