df4156afc514fafadf594b0cf2b54082f64df8f7
Juan F. Pérez <761794+jguitar@users.noreply.github.com>
2024-07-01 17:28:32 +0200

    remove Flutter

--- zsh.after/dev_tools.zsh
+++ zsh.after/dev_tools.zsh
@@ -4,11 +4,6 @@ eval "$(rbenv init - zsh)"
 # Some fix: https://blog.phusion.nl/2017/10/13/why-ruby-app-servers-break-on-macos-high-sierra-and-what-can-be-done-about-it/
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-# Flutter and tools (e.g. Webdev)
-export PATH=$PATH:~/Documents/source/flutter/bin
-export PATH=$PATH:~/Documents/source/flutter/bin/cache/dart-sdk/bin
-export PATH=$PATH:$HOME/.pub-cache/bin
-
 # nvm setup
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
